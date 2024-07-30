0937f876f2ec8a44bc32f1b783fe8377145c8263
Sherie Chandler <SherieC@tlgcohort.com>
2024-07-30 16:31:47 -0400

    updated the enum to be able to specify the category by number

--- src/gov/trivia/model/Category.java
+++ src/gov/trivia/model/Category.java
@@ -1,5 +1,27 @@
 package gov.trivia.model;
 
-enum Category {
-    SPORTS, HISTORY, MUSIC, JAYISMS
+public enum Category {
+    SPORTS(1),
+    HISTORY(2),
+    MUSIC(3),
+    JAYISMS(4);
+
+    private final int id;
+
+    Category(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static Category fromNumber(int number) {
+        for (Category category : Category.values()) {
+            if (category.getId() == number) {
+                return category;
+            }
+        }
+        throw new IllegalArgumentException("Invalid category number: " + number);
+    }
 }
