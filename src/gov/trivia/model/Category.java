5f71abc9aaa3cc4cd739ccb938b73acbeab1442b
Sherie Chandler <SherieC@tlgcohort.com>
2024-07-30 17:06:06 -0400

    test

--- src/gov/trivia/model/Category.java
+++ src/gov/trivia/model/Category.java
@@ -1,8 +1,27 @@
-package gov.trivia.model;
+package gov.quizwiz.model;
 
 public enum Category {
-    SPORTS,
-    HISTORY,
-    MUSIC,
-    JAYISMS
+    SPORTS(1),
+    HISTORY(2),
+    MUSIC(3),
+    JAYISMS(4);
+
+    private int id;
+
+    private Category(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static Category fromId(int id) {
+        for (Category category : Category.values()) {
+            if (category.getId() == id) {
+                return category;
+            }
+        }
+        throw new IllegalArgumentException("Invalid category ID: " + id);
+    }
 }
